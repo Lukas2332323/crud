f45ab959930145e8a055a8eb5dd7445194d458b3
Lucas <lucas.urrutia.a@gmail.com>
2024-02-09 16:15:55 -0300

    Primer commit

---
+++ boton.js
@@ -0,0 +1,6 @@
+const btn_login= document.querySelector("#btn_login");
+
+btn_login.addEventListener("click", function () {
+  // Redireccionar a la página deseada
+  window.location.href = "formulario.html";
+});
